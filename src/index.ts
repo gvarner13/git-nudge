8bafc4583a4bd7710ac016178c138dec42601e71
Gary Varner <garysarahvarner@gmail.com>
2024-01-22 21:44:03 -0500

    adding graphql base query to grab github data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  const token = "token-here"; // Replace with your token
+  const query = `
+      query {
+        user(login: "gvarner13") {
+          contributionsCollection {
+            contributionCalendar {
+              totalContributions
+              weeks {
+                contributionDays {
+                  date
+                  contributionCount
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
 
-export default app
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  const data = await response.json();
+  return c.json(data);
+  // return c.text('Hello Hono!')
+});
+
+export default app;
