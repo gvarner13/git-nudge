d65bb3b2434bfd714b9f69ae43581bfe2f4dd4c6
Gary Varner <garysarahvarner@gmail.com>
2024-02-27 21:47:16 -0500

    added top day from the week

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,16 @@ app.get("/", async (c) => {
   const startOfWeek = new Date(currentDate.setDate(firstDayOfWeek));
   const endOfWeek = new Date(currentDate.setDate(lastDayOfWeek));
 
+  const gitDays = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  };
+
   const query = `
        query {
          user(login: "${username}") {
@@ -54,11 +64,27 @@ app.get("/", async (c) => {
       gitData.data.user.contributionsCollection.contributionCalendar
         .totalContributions;
 
+    const dayWithMaxContributions =
+      gitData.data.user.contributionsCollection.contributionCalendar.weeks.reduce(
+        (maxDay, week) => {
+          return week.contributionDays.reduce((maxDay, day) => {
+            return day.contributionCount > maxDay.contributionCount
+              ? day
+              : maxDay;
+          }, maxDay);
+        },
+        { contributionCount: -1 }
+      );
+
+    console.log(dayWithMaxContributions);
+
     const { data, error } = await resend.emails.send({
       from: "Git Nudge <gitnudge@updates.garyvarner.me>",
       to: ["garysarahvarner@gmail.com"],
       subject: "Weekly Update",
-      html: `You made <strong>${totalContributions}</strong> contributions this week 💪`,
+      html: `You made <strong>${totalContributions}</strong> contributions this week 💪 and your best day was ${
+        gitDays[dayWithMaxContributions.weekday]
+      }.`,
     });
   } catch (error) {
     console.log(error);
